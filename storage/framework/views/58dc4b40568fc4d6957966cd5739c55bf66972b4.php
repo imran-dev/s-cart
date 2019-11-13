ac726cec36da9a9e5d919c547ea0752eb2ab85ca
Imran Hossain <imran.dev.bd@gmail.com>
2019-11-14 03:50:05 +0600

    framework folder removed

--- storage/framework/views/58dc4b40568fc4d6957966cd5739c55bf66972b4.php
+++
@@ -1,6 +0,0 @@
-<!--main right-->
-<div class="col-sm-9 padding-right">
-   <?php echo $__env->yieldContent('center'); ?>
-</div>
-<!--//main right-->
-<?php /**PATH E:\laragon\www\anytimebazar\resources\views/templates/tz-template/center.blade.php ENDPATH**/ ?>
--- storage/framework/views/c59e882db084a32b1589efe6cb5daec5c7c0dd11.php
+++
@@ -1,6 +0,0 @@
-<!--main right-->
-<div class="col-sm-9 padding-right">
-   <?php echo $__env->yieldContent('center'); ?>
-</div>
-<!--//main right-->
-<?php /**PATH E:\laragon\www\s-cart\resources\views/templates/default/center.blade.php ENDPATH**/ ?>
